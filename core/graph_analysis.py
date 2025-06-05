1b737e1af9c80bf0a611fa6d2776ee9e5b59ffb0
bmap-ai <moongirlproject@gmail.com>
2025-06-05 17:45:26 +0900

    feat: Add build_graph method

--- core/graph_analysis.py
+++ core/graph_analysis.py
@@ -7,3 +7,12 @@ import networkx as nx
 class GraphAnalyzer:
     def __init__(self):
         self.graph = nx.DiGraph()
+        
+    def build_graph(self, transactions):
+        """Build transaction graph"""
+        for tx in transactions:
+            from_addr = tx.get("from")
+            to_addr = tx.get("to")
+            if from_addr and to_addr:
+                self.graph.add_edge(from_addr, to_addr, weight=tx.get("value", 0))
+        return self.graph
