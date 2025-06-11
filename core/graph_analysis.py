4207f3564e82678f2e305c38eaac6e2d8beb480c
bmap-ai <moongirlproject@gmail.com>
2025-06-11 16:56:09 +0900

    feat: Add PageRank analysis

--- core/graph_analysis.py
+++ core/graph_analysis.py
@@ -16,3 +16,15 @@ class GraphAnalyzer:
             if from_addr and to_addr:
                 self.graph.add_edge(from_addr, to_addr, weight=tx.get("value", 0))
         return self.graph
+        
+    def find_important_nodes(self, top_n=5):
+        """Find most important nodes by PageRank"""
+        if not self.graph:
+            return []
+        
+        try:
+            pagerank = nx.pagerank(self.graph)
+            sorted_nodes = sorted(pagerank.items(), key=lambda x: x[1], reverse=True)
+            return sorted_nodes[:top_n]
+        except:
+            return []
